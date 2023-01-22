bbd1678d5ef723d19eddf02784b9b5c0e3eabc0b
baxaaka <jockuhacker@gmail.com>
2023-01-22 16:19:29 +0500

    odf

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 // -------------------ozodbekk------------------------
 
+
+
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -32,7 +34,32 @@ window.onclick = function(event) {
 // --------Baxrullo------
 
 
-import Swiper from 'swiper';
+  import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js' ;
+
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
 
 
 // -----Baxrullo end ------
